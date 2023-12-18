f0cf75dc5c2141d70418c35907e9c1e0bbae5f72
Zegetable <38813761+Zegetable@users.noreply.github.com>
2023-12-18 11:31:37 -0600

    items model update

---
+++ InventoryManager/Data/Migrations/20231218172920_ItemsModelUpdate.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManager.Data.Migrations
+{
+    public partial class ItemsModelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Manufacturer",
+                table: "ItemsModel",
+                newName: "Category");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ItemsModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "ItemsModel",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "ItemsModel");
+
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "ItemsModel",
+                newName: "Manufacturer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ItemsModel",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- InventoryManager/Models/ItemsModel.cs
+++ InventoryManager/Models/ItemsModel.cs
@@ -6,9 +6,10 @@ namespace InventoryManager.Models
     {
         [Key]
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Manufacturer { get; set; }
+        public string Name { get; set; }
+        public string? Brand { get; set; }
 
+        public string? Category { get; set; }//footwear, apparel, equipment, bags, misc
 
         public float Price { get; set; }
         [Required]
